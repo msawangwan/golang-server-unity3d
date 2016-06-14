69b5537b780f06761d7d5694806b3ba125805f0d
msawangwan <misha.sawangwan@gmail.com>
2016-06-13 18:13:07 -0700

    started work on serializer encoding/decoding -- decoder works thus far

--- tcp-web-server.go
+++ tcp-web-server.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	//"io"
 	"log"
 	"net"
 	"os"
@@ -17,6 +20,59 @@ type DataPacket struct {
 
 func (dp DataPacket) parse() string {
 	return string(dp.data[:dp.size])
+}
+
+/* Decodes an array of bytes that represents a data frame, from Network Byte Order into an associated uint32 value and adds them to a slice to be returned. */
+func (dp DataPacket) decodeNBO() []uint32 {
+	decodedFrame := make([]uint32, 256)
+	var start, end, offset = 0, 4, 0
+
+	for i := 0; i < 1024; i++ { // TODO: get the num iterations based on the size of the stream of data
+		if i%4 == 0 {
+			var currentVal uint32
+
+			currentSlice := dp.data[start:end]
+			buf := bytes.NewReader(currentSlice)
+			err := binary.Read(buf, binary.BigEndian, &currentVal)
+
+			if err != nil {
+				log.Println("Failed To Decode DataFrame:", err)
+			}
+
+			decodedFrame[offset] = currentVal
+			offset++
+
+			start += 4
+			end += 4
+		}
+	}
+
+	return decodedFrame
+}
+
+func (dp DataPacket) encodeNBO(decodedDataFrame []uint32) []byte {
+	encodedFrame := make([]byte, 1024) // use len(dp.data)
+	frame := bytes.NewBuffer(encodedFrame)
+	//frame := bytes.NewReader(encodedFrame)
+	offset := 0
+
+	for i := 0; i < 256; i++ { // TODO: see comment in decoding func
+		var currentVal uint32
+
+		currentVal = decodedDataFrame[i]
+		//buf := make([]byte, 4)
+		buf := new(bytes.Buffer)
+		err := binary.Write(buf, binary.BigEndian, currentVal)
+
+		if err != nil {
+			log.Println("Failed To Encode DataFrame:", err)
+		}
+
+		frame.WriteAt(buf, offset)
+		offset += 4
+	}
+
+	return frame.Bytes()
 }
 
 func init() {
@@ -54,19 +110,19 @@ func handleClientStream(conn net.Conn) {
 	defer conn.Close()
 
 	readStream(conn)
-	log.Println("Terminating Client Connection")
+	log.Println("Terminating Client Connection ...")
 }
 
 /* Reads data from a stream once a client establishes a connection. */
 func readStream(conn net.Conn) {
-	log.Println("Opening Stream For Read ... ")
+	log.Println("Opening Stream For Read/Write ...")
 
 	readChan := make(chan DataPacket)
 	errChan := make(chan error)
 
 	go func(readChan chan DataPacket, errChan chan error) {
 		for {
-			log.Println("Reading Stream ....")
+			log.Println("Waiting For Client Activity ...")
 
 			data := make([]byte, 1024)
 			dataSize, err := conn.Read(data)
@@ -79,7 +135,7 @@ func readStream(conn net.Conn) {
 			}
 
 			readChan <- dp
-			go writeStream(conn)
+			//go writeStream(conn, dp)
 		}
 	}(readChan, errChan)
 
@@ -88,8 +144,10 @@ func readStream(conn net.Conn) {
 	for {
 		select { // study selecting channels
 		case dp := <-readChan:
-			dataString := dp.parse()
-			log.Println("Data Buffer Read:", dataString)
+			dataFrame := dp.decodeNBO()
+			log.Println("Data Buffer Read:", dataFrame)
+			reEncoded := dp.encodeNBO(dataFrame)
+			go writeStream(conn, reEncoded)
 		case err := <-errChan:
 			isReadComplete = true
 			log.Println("Closing Read Stream:", err)
@@ -106,17 +164,12 @@ func readStream(conn net.Conn) {
 	}
 }
 
-var hasSentOneMsg = false
-
-func writeStream(conn net.Conn) {
-	if hasSentOneMsg == false {
-		log.Println("Writing to stream ... ")
-		msg := "This is the server. Only the server can write this reply. If the server is not replying, you will not see this server reply. End of the Message.Te \r\n"
-		log.Println(len(msg))
-		//writeChan := make(chan string)
-		//writeChan <- msg
-		conn.Write([]byte(msg))
-		conn.Write([]byte(msg))
-		//hasSentOneMsg = true
-	}
+func writeStream(conn net.Conn, dataFrame []byte) {
+	log.Println("Writing to stream ... ")
+	//msg := "This is the server. Only the server can write this reply. If the server is not replying, you will not see this server reply. End of the Message.Te \r\n"
+	//log.Println(len(msg))
+	//writeChan := make(chan string)
+	//writeChan <- msg
+	//conn.Write([]byte(msg))
+	conn.Write(dataFrame)
 }
